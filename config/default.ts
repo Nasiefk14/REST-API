c6f716fa8ea9556b617c6981d6144a00e24d7507
Nasief Khan <nasiefk14@gmail.com>
2023-03-23 14:14:45 +0200

    Updated Routes & Defaults

--- config/default.ts
+++ config/default.ts
@@ -2,7 +2,9 @@ require('dotenv').config();
 
 const DbPort = process.env.PORT
 const DbConnection = process.env.CONNECTIONSTRING
+const SaltWorkVal = process.env.SALTWORKVALUE
 export default {
     port: DbPort,
-    dbUri: DbConnection
+    dbUri: DbConnection,
+    saltWorkFactor: SaltWorkVal
 }      
